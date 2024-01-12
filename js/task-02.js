d2e8ff410a07e4a6717f02db7f876a03fa3fa43f
MarekLempart <kama.zakupy@gmail.com>
2024-01-12 14:45:07 +0100

    Praca domowa nr 6 - zadanie 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
+const ingredientsList = document.querySelector("#ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach((ingredients) => {
+  const li = document.createElement("li");
+  li.textContent = ingredients;
+  li.classList.add("item");
+  ingredientsList.appendChild(li);
+});
